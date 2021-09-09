c4f2c0b0b908f3974055607edfb70cc521fd4a78
EV-02 <ayeekee@knights.ucf.edu>
2021-09-08 20:53:33 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 10 Solution
+ *  Copyright 2021 Alyssa Yee-Kee
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner se = new Scanner(System.in);
+
+        System.out.print("How many euros are you exchanging? ");
+        String numEuro = se.nextLine();
+        double euro = Double.parseDouble(numEuro);
+
+        System.out.print("What is the exchange rate? ");
+        String exRate = se.nextLine();
+        double rate = Double.parseDouble(exRate);
+
+        double dollars = euro * rate;
+
+        System.out.println(String.format("%.0f", euro) + " euros at an exchange rate of " + rate + " is\n" + String.format("%.2f", dollars) + " U.S. dollars.");
+    }
+}
